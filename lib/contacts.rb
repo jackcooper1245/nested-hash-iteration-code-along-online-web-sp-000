23c1243362dc1fad43441b414d709d0d1e0c7a54
Jack Cooper <jackcooper1245@gmail.com>
2020-04-12 11:08:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,17 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, data|
-  if person == "Freddy Mercury"
-    person.each do |attribute, value|
-      if attribute == :favorite_ice_cream_flavors
-        value.delete_if do |flavor|
-          flavor == "strawberry"
-        end
-      end
-    end
-  end
-end
-  remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    binding.pry
+    
 end
   
